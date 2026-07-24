62f054920a6465f83796f076c50eac80cb3e0c40
MOIYS <139212911+MOIYS@users.noreply.github.com>
2026-07-24 13:37:21 +0900

    fix: MyBatis Mapper 스캔 설정 수정 (#9)
    * fix: MyBatis Mapper 스캔 설정 수정
    
    - 스캔은 어노테이션만 가능하게 변경
    - MemberMapper에 @Mapper 추가
    - pom.xml에 spring-test 중복 삭제
    
    * fix: pom.xml 충돌 해결 오타 수정 및 slf4j
    
    * fix: root-context 충돌 해결 오타 수정

--- backend/src/main/java/com/wallet/member/mapper/MemberMapper.java
+++ backend/src/main/java/com/wallet/member/mapper/MemberMapper.java
@@ -1,7 +1,10 @@
 package com.wallet.member.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.wallet.member.domain.Member;
 
+@Mapper
 public interface MemberMapper {
     Member findByEmail(String email);
 }
